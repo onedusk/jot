cdf4764c91f09c141ec53c6cd1438e9b55f7e6ed
OneDusk <one@dusk-labs.com>
2025-10-09 14:32:45 -0400

    .

--- scripts/internal/ghub.sh
+++ scripts/internal/ghub.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
 # echo "# jot" >> README.md
-git init;
+git s-p onedusk;
 git add -A;
-git commit -m "first commit";
+git commit -m ".";
 git branch -M main;
-git remote add origin https://github.com/onedusk/jot.git;
+# git remote add origin git@github.com:onedusk/jot.git;
 git push -u origin main;
